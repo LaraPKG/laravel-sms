02fefa6c9c865b4d3c0c650cde555fbaa94ad92b
Ron Appleton <ron.appleton@gmail.com>
2022-03-22 23:40:50 +0000

    Enable dummy driver and code coverage report.

--- config/laravel-sms.php
+++ config/laravel-sms.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'default_driver' => '',
+    'default_driver' => 'dummy',
 
     'drivers' => [
-
+        'dummy' => \LaraPkg\LaravelSms\Factories\DummyFactory::class
     ]
 ];
